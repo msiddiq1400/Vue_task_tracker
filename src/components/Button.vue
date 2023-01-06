21a29a15972f4dfa1b7583cf03dca6bc33ccfd71
siddiq <msiddiq1400@gmail.com>
2023-01-06 21:49:41 +0500

    task tracker

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button 
+        :style="{background: color}" 
+        class="btn"
+        @click="onClick()">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: {
+            type: String,
+            default: 'Add Task Default'
+        },
+        color: String
+    },
+    methods: {
+        onClick() {
+            console.log("button clicked")
+        }
+    }
+}
+</script>
